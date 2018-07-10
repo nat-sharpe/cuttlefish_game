82362fd11dd3589143254c8cbd90a18ac30dfe73
Nat Sharpe <sharpe1890@gmail.com>
2018-07-10 14:57:36 -0400

    tongue stops when it hits fish and retracts but restracts too far

--- game.py
+++ game.py
@@ -1,10 +1,5 @@
 import pygame
-pygame.init()
-WIDTH = 1000
-DEPTH = 600
-screen = pygame.display.set_mode((WIDTH, DEPTH))
-clock = pygame.time.Clock()
-keystate = pygame.key.get_pressed()
+import random
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, fat, tall, x, y):
@@ -48,6 +43,7 @@ class Tongue(pygame.sprite.Sprite):
         self.speed = 2
         self.dist_extend = 10
         self.dist_retract = 1 + (self.dist_extend * 2)
+        self.hit = False
 
     def update(self):
         self.rect = self.image.get_rect()
@@ -58,13 +54,17 @@ class Tongue(pygame.sprite.Sprite):
             self.time += 1
             if self.time == 1:
                 self.extend()
-            elif self.time % self.speed == 0 and self.time <= (self.speed * self.dist_extend):
-                self.extend()
+            elif self.time <= (self.speed * self.dist_extend):
+                if self.hit:
+                   self.retract() 
+                else:
+                    self.extend()
             elif self.time % self.speed == 0 and self.time > (self.speed * self.dist_extend) and self.time <= (self.speed * self.dist_retract):
                 self.retract()
             elif self.time > (self.speed * self.dist_retract):
                 self.attacking = False
                 self.time = 0
+                self.hit = False
 
 
     def attack(self):
@@ -72,43 +72,62 @@ class Tongue(pygame.sprite.Sprite):
         
     def extend(self):
         self.length += 10
-        self.image = pygame.transform.scale(self.image, (self.length, 5))
+        self.image = pygame.transform.scale(self.image, (self.length, 5))            
 
     def retract(self):
-        self.length -= 10
-        self.image = pygame.transform.scale(self.image, (self.length, 5))
+        if self.rect.right > self.player.rect.right:
+            self.length -= 10
+            self.image = pygame.transform.scale(self.image, (self.length, 5))
+        else:
+            self.length = 1
+            self.image = pygame.transform.scale(self.image, (self.length, 5))            
+
+    def bullseye(self):
+        self.hit = True
 
 class Food(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.fatness = 25
         self.tallness = 20
         self.image = pygame.Surface([self.fatness, self.tallness])
         self.image.fill((255, 200, 100))
         self.rect = self.image.get_rect()
-        self.rect.x = 400
-        self.rect.y = 300
+        self.rect.x = x
+        self.rect.y = y
 
     def update(self):   
         pass
 
 def main():
+    pygame.init()
+
+    WIDTH = 1000
+    DEPTH = 600
+
+    screen = pygame.display.set_mode((WIDTH, DEPTH))
+    clock = pygame.time.Clock()
+
+    keystate = pygame.key.get_pressed()
+
+    all_sprites = pygame.sprite.Group()
+    yummies = pygame.sprite.Group()
+    grabbies = pygame.sprite.Group()
+
+    for i in range(30):
+        x = random.randrange(WIDTH)
+        y = random.randrange(DEPTH)
+        fish = Food(x, y)
+        all_sprites.add(fish)
+        yummies.add(fish)
 
     player = Player(40, 40, 200, 400)
     tongue = Tongue(player)
-    fish = Food()
 
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(player)
     all_sprites.add(tongue)
-    all_sprites.add(fish)
+    all_sprites.add(player)
 
-    yummies = pygame.sprite.Group()
-    yummies.add(fish)
-
-    grabbies = pygame.sprite.Group()
     grabbies.add(tongue)
-
 
     running = True
     while running:
@@ -121,11 +140,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT or keys[pygame.K_q]:
                 running = False
-
-        hits = pygame.sprite.groupcollide(grabbies, yummies, False, False)
-
-        if hits:
-            fish.kill()
 
         if keys[pygame.K_LEFT] and player.rect.left > 0:
             player.moving_x(-speed)
@@ -140,6 +154,10 @@ def main():
             tongue.attack()             
 
         all_sprites.update()
+
+        hit = pygame.sprite.groupcollide(grabbies, yummies, False, True)
+        if hit:
+            tongue.bullseye()
 
         screen.fill((0, 0, 0)) 
         all_sprites.draw(screen)
