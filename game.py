bddb4a37eef55580ec975c91e97ece494ec6c2d7
Nat Sharpe <sharpe1890@gmail.com>
2018-07-10 14:31:32 -0400

    test of tongue extending and colliding with fish sprite

--- game.py
+++ game.py
@@ -31,7 +31,7 @@ class Player(pygame.sprite.Sprite):
     def moving_y(self, direction):
         self.move_y = direction
         
-class Other(pygame.sprite.Sprite):
+class Tongue(pygame.sprite.Sprite):
     def __init__(self, player):
         pygame.sprite.Sprite.__init__(self)
         self.fatness = 1
@@ -40,13 +40,17 @@ class Other(pygame.sprite.Sprite):
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
         self.player = player
-        self.length = 0
+        self.length = 1
         self.rect.left = self.player.rect.right
         self.rect.centery = self.player.rect.centery
         self.attacking = False
         self.time = 0
+        self.speed = 2
+        self.dist_extend = 10
+        self.dist_retract = 1 + (self.dist_extend * 2)
 
     def update(self):
+        self.rect = self.image.get_rect()
         self.rect.left = self.player.rect.right
         self.rect.centery = self.player.rect.centery
 
@@ -54,47 +58,74 @@ class Other(pygame.sprite.Sprite):
             self.time += 1
             if self.time == 1:
                 self.extend()
-            elif self.time % 3 == 0 and self.time < 240:
+            elif self.time % self.speed == 0 and self.time <= (self.speed * self.dist_extend):
                 self.extend()
-            elif self.time % 3 == 0 and self.time < 540 and self.length > 1:
+            elif self.time % self.speed == 0 and self.time > (self.speed * self.dist_extend) and self.time <= (self.speed * self.dist_retract):
                 self.retract()
-            elif self.time > 540:
+            elif self.time > (self.speed * self.dist_retract):
                 self.attacking = False
                 self.time = 0
-                self.length = 1
 
 
     def attack(self):
         self.attacking = True
         
     def extend(self):
-        self.length += 1
+        self.length += 10
         self.image = pygame.transform.scale(self.image, (self.length, 5))
 
     def retract(self):
-        self.length -= 1
+        self.length -= 10
         self.image = pygame.transform.scale(self.image, (self.length, 5))
+
+class Food(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.fatness = 25
+        self.tallness = 20
+        self.image = pygame.Surface([self.fatness, self.tallness])
+        self.image.fill((255, 200, 100))
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 300
+
+    def update(self):   
+        pass
 
 def main():
 
     player = Player(40, 40, 200, 400)
-    tongue = Other(player)
+    tongue = Tongue(player)
+    fish = Food()
 
     all_sprites = pygame.sprite.Group()
     all_sprites.add(player)
     all_sprites.add(tongue)
-    
+    all_sprites.add(fish)
+
+    yummies = pygame.sprite.Group()
+    yummies.add(fish)
+
+    grabbies = pygame.sprite.Group()
+    grabbies.add(tongue)
+
+
     running = True
     while running:
         
         clock.tick(60)
-        speed = 10
+        speed = 2
 
         keys = pygame.key.get_pressed()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT or keys[pygame.K_q]:
                 running = False
+
+        hits = pygame.sprite.groupcollide(grabbies, yummies, False, False)
+
+        if hits:
+            fish.kill()
 
         if keys[pygame.K_LEFT] and player.rect.left > 0:
             player.moving_x(-speed)
